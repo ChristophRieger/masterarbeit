05d1a8206ea86d500072798e91392e307eb92a97
Christoph Rieger <christoph.rieger@outlook.com>
2023-03-15 17:58:04 +0100

    updated generation of vertHorz line images

--- code/verticalHorizontalNetwork/verticalHorizontalLineTester.py
+++ code/verticalHorizontalNetwork/verticalHorizontalLineTester.py
@@ -4,15 +4,28 @@ Created on Sun Mar 12 09:56:48 2023
 
 @author: chris
 """
+import sys
+sys.path.insert(0, '../helpers')
 
 import dataGenerator
 import matplotlib.pyplot as plt
 
+plt.close("all")
 
-image, xPosition = dataGenerator.generateRandomVerticalLineImage()
+# generate vertical images
+image, xPosition, prior = dataGenerator.generateRandomVerticalLineImage()
 plt.figure()
 plt.imshow(image, cmap='gray')
 
-image, yPosition = dataGenerator.generateRandomHorizontalLineImage()
+image, xPosition, prior = dataGenerator.generateVerticalLineImage(29)
+plt.figure()
+plt.imshow(image, cmap='gray')
+
+# generate horizontal images
+image, yPosition, prior = dataGenerator.generateRandomHorizontalLineImage()
+plt.figure()
+plt.imshow(image, cmap='gray')
+
+image, yPosition, prior = dataGenerator.generateHorizontalLineImage(0)
 plt.figure()
 plt.imshow(image, cmap='gray')
