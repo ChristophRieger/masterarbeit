fe16e751d615f3ba2459deb90f518d705e1d5808
Christoph Rieger <christoph.rieger@outlook.com>
2023-04-17 11:43:13 +0200

    progress of protocol-of-work-so-far

--- code/verticalHorizontalNetwork/showGeneratedImages.py
+++ code/verticalHorizontalNetwork/showGeneratedImages.py
@@ -12,6 +12,20 @@ import matplotlib.pyplot as plt
 
 plt.close("all")
 
+fig, (ax1, ax2) = plt.subplots(1,2)
+
+fig.suptitle('Examples of training images', fontsize=14)
+fig.subplots_adjust(top=1.1)
+image1, xPosition, prior, orientation = dataGenerator.generateVerticalLineImage(12)
+image2, yPosition, prior, orientation = dataGenerator.generateHorizontalLineImage(18)
+# Line plots
+ax1.set_title('Image in vertical orientation')
+ax1.imshow(image1, cmap='gray')
+ax2.set_title('Image in horizontal orientation')
+ax2.imshow(image2, cmap='gray')
+
+
+
 # generate vertical images
 image, xPosition, prior, orientation = dataGenerator.generateRandomVerticalLineImage()
 plt.figure()
@@ -30,6 +44,6 @@ plt.imshow(image, cmap='gray')
 # plt.figure()
 # plt.imshow(image, cmap='gray')
 
-image, xPosition, yPosition, orientation = dataGenerator.generateRandomCrossLineImage()
+image, position, orientation = dataGenerator.generateRandomCrossLineImage()
 plt.figure()
 plt.imshow(image, cmap='gray')
