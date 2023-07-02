9a9f334c195b5d5c99281902c3466e45248c444d
Christoph Rieger <christoph.rieger@outlook.com>
2023-07-02 11:40:19 +0200

    commented out unneeded function

--- code/helpers/neuronFunctions.py
+++ code/helpers/neuronFunctions.py
@@ -27,25 +27,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-# just plotting the double exp function
-plt.close("all")
-# Define the function y(x)
-def y(x):
-  # c needs to be dependent on w which are constantly changing or very big.. paper lies that c > 1 is sufficient, its wrong
-  # c = 5
-  # the middle term goes up to about 0.8
-  # return (c * np.exp(-0.5)) * (np.exp(-x/0.015) - np.exp(-x/0.001)) - 1
-  return np.exp(-x/0.015) - np.exp(-x/0.001)
+# # just plotting the double exp function
+# plt.close("all")
+# # Define the function y(x)
+# def y(x):
+#   # c needs to be dependent on w which are constantly changing or very big.. paper lies that c > 1 is sufficient, its wrong
+#   # c = 5
+#   # the middle term goes up to about 0.8
+#   # return (c * np.exp(-0.5)) * (np.exp(-x/0.015) - np.exp(-x/0.001)) - 1
+#   return np.exp(-x/0.015) - np.exp(-x/0.001)
 
-# Generate x values from 0 to 1 with a step of 0.01
-x = np.arange(0, 0.05, 0.0001)
+# # Generate x values from 0 to 1 with a step of 0.01
+# x = np.arange(0, 0.05, 0.0001)
 
-# Evaluate y(x) for each value of x
-y_values = y(x)
+# # Evaluate y(x) for each value of x
+# y_values = y(x)
 
-# Plot the function
-plt.plot(x, y_values)
-plt.xlabel("t [s]")
-plt.ylabel("Amplitude")
-plt.title("EPSP with a double exponential kernel")
-plt.show()
+# # Plot the function
+# plt.plot(x, y_values)
+# plt.xlabel("t [s]")
+# plt.ylabel("Amplitude")
+# plt.title("EPSP with a double exponential kernelll")
+# plt.show()
