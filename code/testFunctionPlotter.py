2ea06437028901b3924f170cb23df313f310d122
Christoph Rieger <christoph.rieger@outlook.com>
2024-09-28 20:31:53 +0200

    Correction according to Legi's 1st review

--- code/testFunctionPlotter.py
+++ code/testFunctionPlotter.py
@@ -12,19 +12,28 @@ import numpy as np
 # just plotting the double exp function
 plt.close("all")
 # Define the function y(x)
-def y(x):
-  # c needs to be dependent on w which are constantly changing or very big.. paper lies that c > 1 is sufficient, its wrong
-  c = 5
-  # the middle term goes up to about 0.8
-  # return (c * np.exp(-0.5)) * (np.exp(-x/0.015) - np.exp(-x/0.001)) - 1
-  return np.e ** -x
+def y(x):  
+  return (np.exp(-x/0.015) - np.exp(-x/0.001))
+  # return np.e ** -x
 
 # Generate x values from 0 to 1 with a step of 0.01
-x = np.arange(1, 5, 0.0001)
+x = np.arange(-0.01, 0.05, 0.0001)
 
 # Evaluate y(x) for each value of x
 y_values = y(x)
 
+i = 0
+while x[i] < 0:
+  y_values[i] = 0
+  i = i + 1
+
 # Plot the function
 plt.plot(x, y_values)
+plt.title("Kernel function", fontsize=14)
+plt.xlabel("s", fontsize=15)
+plt.xticks(fontsize=11)
+
+plt.ylabel(r'$\varepsilon (s)$', fontsize=15)
+plt.yticks(fontsize=11)
+
 plt.show()
